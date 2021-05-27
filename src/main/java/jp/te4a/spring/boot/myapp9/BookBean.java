deffad83e6adf86152f888106cbd31cbe6b2174b
te180453 <te180453jc21@gmail.com>
2021-05-27 14:19:04 +0900

    エンティティ化

--- src/main/java/jp/te4a/spring/boot/myapp9/BookBean.java
+++ src/main/java/jp/te4a/spring/boot/myapp9/BookBean.java
@@ -1,14 +1,20 @@
-package jp.te4a.spring.boot.myapp8;
+package jp.te4a.spring.boot.myapp9;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import javax.persistence.*;
 
+@Entity //エンティティ化？
+@Table(name="books") //テーブル
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class BookBean{
+    @Id
+    @GeneratedValue
     Integer id;
+    @Column(nullable = false)
     String title;
     String writter;
     String publisher;
