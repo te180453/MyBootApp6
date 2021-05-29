cefefb85fe6ac6cda968081ad1f0e10894b506ad
te180453 <te180453jc21@gmail.com>
2021-05-29 18:05:18 +0900

    値の検証してくれないのはなぜでしょう

--- src/main/java/jp/te4a/spring/boot/myapp11/BookController.java
+++ src/main/java/jp/te4a/spring/boot/myapp11/BookController.java
@@ -1,9 +1,11 @@
-package jp.te4a.spring.boot.myapp9;
+package jp.te4a.spring.boot.myapp11;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,31 +21,38 @@ public class BookController {
     BookServise bookService;
 
     @ModelAttribute
-    BookForm setUpForm(){
+    BookForm setUpForm() {
         return new BookForm();
     }
 
     @GetMapping
-    String list(Model model){
+    String list(Model model) {
         model.addAttribute("books", bookService.findAll());
         return "books/list";
     }
 
-    @PostMapping(path="create")
-    String create(BookForm form, Model mode) {
+    @PostMapping(path = "create")
+    String create(@Validated BookForm form, BindingResult result, Model model) {
+        System.out.println(form);
+        if(result.hasErrors()){
+            return list(model);
+        }
         bookService.create(form);
         return "redirect:/books";
     }
 
-    @PostMapping(path = "edit", params= "form")
+    @PostMapping(path = "edit", params = "form")
     String editForm(@RequestParam Integer id, BookForm form) {
         BookForm bookForm = bookService.findOne(id);
-        BeanUtils.copyProperties(bookForm,  form);
+        BeanUtils.copyProperties(bookForm, form);
         return "books/edit";
     }
 
     @PostMapping(path = "edit")
-    String edit(@RequestParam Integer id, BookForm form) {
+    String edit(@RequestParam Integer id, @Validated BookForm form,BindingResult result) {
+        if(result.hasErrors()){
+            return editForm(id, form);
+        }
         bookService.update(form);
         return "redirect:/books";
     }
@@ -54,9 +63,9 @@ public class BookController {
         return "redirect:/books";
     }
 
-    @PostMapping(path = "edit", params= "goToTop")
+    @PostMapping(path = "edit", params = "goToTop")
     String goToTop() {
         return "redirect:/books";
     }
-    
+
 }
