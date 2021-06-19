e3417a977b2f60b7d13c49eba8a002f8d8eea126
te180453 <te180453jc21@gmail.com>
2021-06-19 21:41:52 +0900

    テスト作成

---
+++ src/test/java/jp/te4a/spring/boot/myapp8/BookForm_Test.java
@@ -0,0 +1,14 @@
+package jp.te4a.spring.boot.myapp8;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+// BookForm_Testの実装
+public class BookForm_Test{
+    @Test
+    public void bookFormを作る(){
+        BookForm bf = new BookForm();
+        assertTrue(bf.getClass() == BookForm.class);
+    }
+}
